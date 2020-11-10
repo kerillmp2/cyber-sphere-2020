6ff88a8dc41afd4ed7387965c4772ef2d4e72f09
kerillmp2 <zebynda@gmail.com>
2020-11-10 16:31:38 +0300

    alm

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-//kekwait
+//keku
 Amplify.configure(config);
 
 ReactDOM.render(
